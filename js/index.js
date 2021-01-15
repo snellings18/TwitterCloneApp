44e71991f478438c19f860ccb53c3e5157d3c46b
Larry Snellings <larry.snellingsjr@gmail.com>
2021-01-15 16:52:14 -0700

    Add profile name, handle, and image; add vidoe/gif/image for tweets.

--- js/index.js
+++ js/index.js
@@ -18,6 +18,7 @@ const getTwitterData = () => {
         return Response.json();
     }).then((data)=>{
         buildTweets(data.statuses);
+        console.log(data);
     })
 
 }
@@ -49,21 +50,26 @@ const buildTweets = (tweets, nextPage) => {
         twitterContent += `
         <div class="tweet-container">
             <div class="tweet-user-info">
-                <div class="tweet-user-profile">
+                <div class="tweet-user-profile" style="background-image: url(${tweet.user.profile_image_url})">
+                    
+                </div>
+                <div class="tweet-user-name-container">
+                    <div class="tweet-user-fullname">
+                        ${tweet.user.name}
+                    </div>
+                    <div class="tweet-user-username">
+                        ${tweet.user.screen_name}
+                    </div>
+                </div>
+            </div>
             
-                </div>
-            </div>
-            <div class="tweet-user-name-container">
-                <div class="tweet-user-fullname">
-                    Larry Snellings Jr.
-                </div>
-                <div class="tweet-user-username">
-                    @Snellings2000
-                </div>
-            </div>
-            <div class="tweet-images-container">
-                <div class="tweet-image"></div>
-            </div>
+        `
+        if( tweet.extended_entities &&
+            tweet.extended_entities.media.length > 0){
+            twitterContent += buildImages(tweet.extended_entities.media);
+            twitterContent += buildVideo(tweet.extended_entities.media);
+        }
+        twitterContent += `
             <div class="tweet-text-container">
                 ${tweet.full_text}
             </div>
@@ -81,12 +87,41 @@ const buildTweets = (tweets, nextPage) => {
  * Build HTML for Tweets Images
  */
 const buildImages = (mediaList) => {
-
+    let imagesContent = `<div class="tweet-images-container">`;
+    let imageExists = false;
+    mediaList.map((media)=>{
+        if(media.type == "photo"){
+            imageExists = true;
+            imagesContent += `<div class="tweet-image" style="background-image: url(${media.media_url_https})"></div>`;
+        }
+    });
+    imagesContent += `</div>`
+    return imageExists ? imagesContent : '';
 }
 
 /**
  * Build HTML for Tweets Video
  */
 const buildVideo = (mediaList) => {
-
+    let videoContent = `<div class="tweet-video-container">`;
+    let videoExists = false;
+    mediaList.map((media)=>{
+        if(media.type == "video"){
+            videoExists = true;
+            videoContent += `
+            <video controls>
+                <source src="${media.video_info.variants[0].url}" type="video/mp4">
+            </video>
+            `
+        } else if(media.type == "animated_gif"){
+            videoExists = true;
+            videoContent += `
+            <video loop autoplay>
+                <source src="${media.video_info.variants[0].url}" type="video/mp4">
+            </video>
+            ` 
+        }
+    });
+    videoContent += `</div>`
+    return videoExists ? videoContent : '';
 }
