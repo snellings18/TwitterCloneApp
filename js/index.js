c5dcf49aa0ec2865664e245e5ab1741a5ffd5156
Larry Snellings <larry.snellingsjr@gmail.com>
2021-01-15 19:16:05 -0700

    Add next-page functionality, add profile picture, further styling.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,36 @@
 const URL = "http://localhost:3000/tweets";
 
+let nextPageUrl = null;
+
 const onEnter = (e) => {
     if(e.key == "Enter") {
         getTwitterData(e);
     }
 }
 
+const onNextPage = () => {
+    if(nextPageUrl){
+        getTwitterData(true);
+    }
+}
+
 /**
  * Retrive Twitter Data from API
  */
-const getTwitterData = () => {
+const getTwitterData = (nextPage=false) => {
     const query = document.getElementById('user-search-input').value;
     if(!query) return;
     const encodedQuery = encodeURIComponent(query);
-    const fullURL = `${URL}?q=${encodedQuery}&count=0`;
+    let fullURL = `${URL}?q=${encodedQuery}&count=0`;
+    if(nextPage && nextPageUrl){
+        fullURL = nextPageUrl;
+    }
     fetch(fullURL).then((Response)=>{
         return Response.json();
     }).then((data)=>{
-        buildTweets(data.statuses);
-        console.log(data);
+        buildTweets(data.statuses, nextPage);
+        saveNextPage(data.search_metadata);
+        nextPageButtonVisibility(data.search_metadata);
     })
 
 }
@@ -27,18 +39,31 @@ const getTwitterData = () => {
  * Save the next page data
  */
 const saveNextPage = (metadata) => {
+    if(metadata.next_results) {
+        nextPageUrl = `${URL}${metadata.next_results}`;
+    } else {
+        nextPageUrl = null;
+    }
 }
 
 /**
  * Handle when a user clicks on a trend
  */
 const selectTrend = (e) => {
+    const text = e.innerText;
+    document.getElementById("user-search-input").value = text;
+    getTwitterData();
 }
 
 /**
  * Set the visibility of next page based on if there is data on next page
  */
 const nextPageButtonVisibility = (metadata) => {
+    if(metadata.next_results){
+        document.getElementById('next-page').style.visibility = "visible"
+    } else {
+        document.getElementById('next-page').style.visibility = "hidden"
+    }
 }
 
 /**
@@ -47,6 +72,7 @@ const nextPageButtonVisibility = (metadata) => {
 const buildTweets = (tweets, nextPage) => {
     let twitterContent = "";
     tweets.map((tweet)=>{
+        const createdDate = moment(tweet.created_at).fromNow();
         twitterContent += `
         <div class="tweet-container">
             <div class="tweet-user-info">
@@ -72,15 +98,20 @@ const buildTweets = (tweets, nextPage) => {
         twitterContent += `
             <div class="tweet-text-container">
                 ${tweet.full_text}
-            </div>
-            <div class="tweet-date-container">
-                20 hours ago
+                </div>
+                <div class="tweet-date-container">
+                ${createdDate}
             </div>
         </div>
         `
     })
+    
+    if(nextPage){
+        document.querySelector('.tweets-list').insertAdjacentHTML('beforeend', twitterContent);
+    } else {
+        document.querySelector('.tweets-list').innerHTML = twitterContent;
+    }
 
-    document.querySelector('.tweets-list').innerHTML = twitterContent;
 }
 
 /**
@@ -108,16 +139,18 @@ const buildVideo = (mediaList) => {
     mediaList.map((media)=>{
         if(media.type == "video"){
             videoExists = true;
+            const videoVariant = media.video_info.variants.find((variant)=>variant.content_type == 'video/mp4');
             videoContent += `
             <video controls>
-                <source src="${media.video_info.variants[0].url}" type="video/mp4">
+            <source src="${videoVariant.url}" type="video/mp4">
             </video>
             `
         } else if(media.type == "animated_gif"){
             videoExists = true;
+            const videoVariant = media.video_info.variants.find((variant)=>variant.content_type == 'video/mp4');
             videoContent += `
             <video loop autoplay>
-                <source src="${media.video_info.variants[0].url}" type="video/mp4">
+                <source src="${videoVariant.url}" type="video/mp4">
             </video>
             ` 
         }
